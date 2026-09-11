12ea9413079292c0677950b22ccccc829e5cfd11
LuisGT <luis.gomez.trujillo@gmail.com>
2026-09-11 18:41:08 -0500

    CHANGE: allow_origin_regex to production url at main.py

--- app/main.py
+++ app/main.py
@@ -35,8 +35,8 @@ app = FastAPI(title="Lotería del Risaralda API", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     # allow_origin_regex=r"https?://(localhost:3000|(?:loteria-risaralda)(-git-[\w-]+-[\w]+)?\.vercel\.app)",
-    allow_origin_regex=r"https?://(localhost:3000)",
-    # allow_origin_regex=r"https?://(loteria-risaralda)",
+    # allow_origin_regex=r"https?://(localhost:3000)",
+    allow_origin_regex=r"https?://(loteria-risaralda)",
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
